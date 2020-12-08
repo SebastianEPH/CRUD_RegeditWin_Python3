90cf226bc1cb5bce925830dcc3f6b26f8285567c
sebastianeph <sebastianeph99@gmail.com>
2020-12-07 22:59:06 -0500

    new class main

--- Examples.py
+++ Examples.py
@@ -9,32 +9,35 @@ sub_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\"
 key = "Run"
 path = sub_path + key
 
-############### CREATE KEY ################
-def createKey():
+
+def createKey(self):
     # Example: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
     # sub_path can be empty
     winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
 
+
 ############### SET VALUE EX ################
-def setValueEx():
+def setValueEx(self):
     # Example: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
     # Open key
-    opened_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_ALL_ACCESS)    # Error if key is emply
+    opened_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_ALL_ACCESS)  # Error if key is emply
     name_value = "Valor"
     value = "soy un valor"
     # Create value
     winreg.SetValueEx(opened_key, name_value, 0, winreg.REG_EXPAND_SZ, value)
 
+
 ############### SET VALUE ################
-def setValue():
+def setValue(self):
     # Example: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
-    name_value = "Valor"    # not use name value // (default)
+    name_value = "Valor"  # not use name value // (default)
     value = "soy un valor"
     # Create value
-    winreg.SetValue(winreg.HKEY_CURRENT_USER, key ,winreg.REG_SZ,value)
+    winreg.SetValue(winreg.HKEY_CURRENT_USER, key, winreg.REG_SZ, value)
+
 
 ############### DELETE VALUE ################
-def deleteValue():
+def deleteValue(self):
     # Example: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
     # Open key
     opened_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_ALL_ACCESS)  # Error if key is emply
@@ -42,15 +45,37 @@ def deleteValue():
     # Create value
     winreg.DeleteValue(opened_key, name_value)
 
+
 ############### DELETE KEY ################
-def deleteKey():
+def deleteKey(self):
     # This method can not delete keys with subkeys.
-    winreg.DeleteKey(winreg.HKEY_CURRENT_USER,path)
+    winreg.DeleteKey(winreg.HKEY_CURRENT_USER, path)
+
 
 ############### DELETE KEY  ################
-def deleteKeyEx():
+def deleteKeyEx(self):
     # This method can not delete keys with subkeys.
     winreg.DeleteKeyEx(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_ALL_ACCESS)
+
+
+class WinRegistry(path):
+    def __init__(self):
+        self.path = path
+        pass
+    def create_key(self):
+        pass
+    def delete_key(self):
+        pass
+    def read_value(self):
+        pass
+    def create_value(self):
+        pass
+    def delete_value(self):
+        pass
+
+
+
+
 """
 def saveKey():
     # Example: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
@@ -62,10 +87,6 @@ def saveKey():
     #winreg.LoadKey(opened_key, name_value, file)
     winreg.SaveKey(opened_key, file.name)
     """
-
-
-
-
 
 
 # Read Documentation:
